a7cb9afc62c475dd78c26a817c88c0739f85ba34
metabrez <metabrez@gmail.com>
2018-12-10 07:13:12 -0500

    param map added

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-details',
   template: `
     <h4>
      The selected ID is = {{departmentId}} 
+     <br> 
+     <a (click)="goPrevious()">Previous</a>
+    <a (click)="goNext()">Next</a>
     </h4>
   `,
   styles: []
 })
 export class DepartmentDetailsComponent implements OnInit {
 public departmentId;
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
-    let id=parseInt(this.route.snapshot.paramMap.get('id'));
-    this.departmentId=id;
-  }
+    //Snapshot approach 
+   // let id=parseInt(this.route.snapshot.paramMap.get('id'));
+   // this.departmentId=id;
 
+   this.route.paramMap.subscribe((params:ParamMap) =>{
+let id=parseInt(params.get('id'));
+this.departmentId=id;
+   });
+  }
+goPrevious(){
+  let previousId=this.departmentId-1;
+  this.router.navigate(['/departments',previousId]);
+}
+goNext(){
+  let nextId=this.departmentId+1;
+  this.router.navigate(['/departments',nextId])
+}
 }
